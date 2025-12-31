0c0b9b35007ac157684ff163bfe22df2ed7cabf3
Jake Hansen <statefromjakefarm44@gmail.com>
2025-12-31 15:08:00 -0800

    wip

--- src/DenStream.py
+++ src/DenStream.py
@@ -2,6 +2,7 @@ import numpy as np
 import faiss
 
 from MicroCluster import MicroCluster
+from loguru import logger
 
 
 class DenStream:
@@ -11,7 +12,7 @@ class DenStream:
             beta,
             mu,
             epsilon,
-            n_samples_init,
+            n_init_points,
             stream_speed):
 
         # Hyperparameters
@@ -19,7 +20,7 @@ class DenStream:
         self.beta = beta
         self.mu = mu
         self.epsilon = epsilon
-        self.n_samples_init = n_samples_init
+        self.n_init_points = n_init_points
         self.stream_speed = stream_speed
 
         # Internal components
@@ -30,7 +31,18 @@ class DenStream:
         self.init_points = None
         self.initialized = False
 
+        logger.debug(
+            "Model Params:"
+            f"\n\tlambda        = {self.lamb}"
+            f"\n\tbeta          = {self.beta}"
+            f"\n\tmu            = {self.mu}"
+            f"\n\tepsilon       = {self.epsilon}"
+            f"\n\tn_init_points = {self.n_init_points}"
+            f"\n\tstream_speed  = {self.stream_speed}"
+        )
 
+
+    @logger.catch
     def partial_fit(
             self,
             X):
@@ -40,28 +52,43 @@ class DenStream:
             pass
         elif self.init_points is None:
             # This is the first batch of points we've seen
-            self.init_points = X[:self.n_samples_init]
-            X = X[self.n_samples_init:]
-        elif self.init_points.shape[0] < self.n_samples_init:
+            self.init_points = X[:self.n_init_points]
+            X = X[self.n_init_points:]
+        elif self.init_points.shape[0] < self.n_init_points:
             # We need more points before we can initialize this model, so just add
             # these new points to the pool
-            remaining = self.n_samples_init - self.init_points.shape[0]
+            remaining = self.n_init_points - self.init_points.shape[0]
             self.init_points = np.concatenate((self.init_points, X[:remaining]), axis=0)
             X = X[remaining:]
 
-        if not self.initialized and self.init_points.shape[0] == self.n_samples_init:
+        if not self.initialized and self.init_points.shape[0] == self.n_init_points:
+            logger.debug("Got enough points to initialize. Initializing p-micro-clusters...")
+
             # It's time to initialize our p-micro-clusters
             index = faiss.IndexFlatL2(self.init_points.shape[1])
-            unassigned = set(range(self.n_samples_init))
+            index.add(self.init_points)
 
-            for point_idx in unassigned:
+            # Iterate over all points that have not been assigned to a cluster
+            assigned = set()
+            for point_idx, point in enumerate(self.init_points):
+                if point_idx in assigned:
+                    # Point already belongs to a p-micro-cluster
+                    continue
+
+                # Find points in epsilon neighborhood
                 query = np.array([self.init_points[point_idx]])
                 lims, dists, inds = index.range_search(query, epsilon)
-                if len(inds) > 0:
-                    print(lims)
-                    print(dists)
-                    print(inds)
-                    print()
+
+                if len(inds) >= self.beta * self.mu:
+                    # This point and its epsilon neighborhood are heavy enough to be a p-micro-cluster
+                    new_pmc = MicroCluster(self.init_points[inds], 1, self.lamb)
+                    self.pmc.append(new_pmc)
+
+                    # The points of this new p-micro-cluster are now off the table
+                    assigned.update(inds)
+
+            logger.debug(f"Found {len(self.pmc)} potential-micro-clusters after initialization")
+            logger.debug(f"{self.n_init_points - len(assigned)}/{self.n_init_points} points did not get assigned to a potential-micro-cluster")
 
 
 if __name__ == "__main__":
@@ -69,11 +96,11 @@ if __name__ == "__main__":
     lamb = 0.01
     beta = 0.5
     mu = 2.5
-    epsilon = 0.5
-    n_samples_init = 500
+    epsilon = 0.04
+    n_init_points = 200
     stream_speed = 10
 
-    model = DenStream(lamb, beta, mu, epsilon, n_samples_init, stream_speed)
+    model = DenStream(lamb, beta, mu, epsilon, n_init_points, stream_speed)
 
     # Create points
     X = np.random.random((500, 3)).astype('float32')
--- src/MicroCluster.py
+++ src/MicroCluster.py
@@ -4,8 +4,46 @@ import numpy as np
 class MicroCluster:
     def __init__(
             self,
-            first_point,
-            creation_time):
+            points,
+            creation_time,
+            lamb):
 
-        self.points = np.array([first_point])
+        self.points = points
+        self.point_arrival_times = np.array([creation_time for _ in range(points.shape[0])])
+        self.lamb = lamb
         self.t0 = creation_time
+
+        self._update_weight()
+        self._update_center()
+
+
+    def add_points(
+            self,
+            points,
+            point_arrival_time):
+
+        self.points = np.concatenate((self.points, points), axis=0)
+        point_arrival_times = np.array([point_arrival_time for _ in range(points.shape[0])])
+        self.point_arrival_times = np.concatenate((self.point_arrival_times, point_arrival_times), axis=0)
+
+        # Need to keep stats current
+        self._update_weight()
+        self._update_center()
+        #self._update_radius()
+
+
+    def _get_point_fades(self):
+        return np.pow(2, -self.lamb * (self.point_arrival_times - self.t0))
+
+
+    def _update_weight(self):
+        self.weight = np.sum(self._get_point_fades())
+
+
+    def _update_center(self):
+        print(self._get_point_fades())
+        print(self.points)
+        print(np.matmul(self._get_point_fades(), self.points))
+        self.center = np.matmul(self._get_point_fades(), self.points) / self.weight
+        print(self.center)
+        exit(0)
